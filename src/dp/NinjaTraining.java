4f7cdb5540d91f4e4fe42fa6001e75349605fcdc
Eswar-Mani-Teja <102649136+Eswar-Mani-Teja@users.noreply.github.com>
2022-05-05 19:04:51 +0530

    Solved Ninja Training problem with Memoization

--- src/dp/NinjaTraining.java
+++ src/dp/NinjaTraining.java
@@ -1,5 +1,7 @@
 package dp;
 
+import utils.Log;
+
 public class NinjaTraining {
 
     public static void main(String[] args) {
@@ -9,10 +11,15 @@ public class NinjaTraining {
         int numberOfTrainingDays = meritPoints.length;
         int maxMeritPoints = getMaximumMeritPoints(meritPoints, numberOfTrainingDays, -1);
         System.out.println("Maximum Merit Points: " + maxMeritPoints + " [BF]");
+        maxMeritPoints = getMaximumMeritPoints(meritPoints, numberOfTrainingDays, -1,
+                new int[numberOfTrainingDays][meritPoints[0].length]);
+        System.out.println("Maximum Merit Points: " + maxMeritPoints + " [MEMO-F]");
+        Log.callSummaries();
     }
 
     //Brute Force
     private static int getMaximumMeritPoints(int[][] points, int day, int nextAct) {
+        Log.callCounter("bf");
         if (day == -1) return 0;
         int maxPoint = 0;
         for (int act = 0; act < points[0].length; act++) {
@@ -24,5 +31,32 @@ public class NinjaTraining {
             }
         }
         return maxPoint;
+    }
+
+    //Memoization
+    private static int getMaximumMeritPoints(int[][] points, int day, int nextAct, int[][] memo) {
+        Log.callCounter("memo-full");
+        if (day == -1) return 0;
+        if (nextAct != -1) {
+            int merit = memo[day][nextAct];
+            if (merit != 0) {
+                //System.out.printf("memory: (%d, %d): %d%n", day, nextAct, merit);
+                return merit;
+            }
+        }
+        int maxMeritPoint = 0;
+        for (int act = 0; act < points[0].length; act++) {
+            if (act != nextAct) {
+                int current = nextAct != -1 ? points[day][act] : 0;
+                int meritPoint = getMaximumMeritPoints(points, day - 1, act, memo) + current;
+                if (meritPoint > maxMeritPoint)
+                    maxMeritPoint = meritPoint;
+            }
+        }
+        if (nextAct != -1) {
+            //System.out.printf("function: (%d, %d): %d%n", day, nextAct, maxMeritPoint);
+            memo[day][nextAct] = maxMeritPoint;
+        }
+        return maxMeritPoint;
     }
 }
